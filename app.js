a44f3f8a68c757c1d3ead0f0b25dd4744685ce42
roshankumar <roshan.k@toppr.com>
2022-12-28 22:35:57 +0530

    completed first assignment

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const http =require('http');
-const routes=require('./routes')
+const http= require('http');
+const routes=require('./routes');
 
-const server=http.createServer(routes)
-
-server.listen(8080);
+const server=http.createServer(routes);
+server.listen(3000);
--- routes.js
+++ routes.js
@@ -1,34 +1,39 @@
 
-const fs=require('fs')
-
-const requestHandler=(req,res)=>{
-    const url =req.url;
+const serverRequestHandler=(req,res)=>{
+    const url= req.url;
     const method=req.method;
-    if(url==='/'){
+    if(url=='/'){
+            res.write('<html>');
+            res.write('<head><title>Hi</title></head>');
+            res.write('<body>Hello from fuber</body>');
+            res.write('<form action="/create-user" method="POST"><label for="username">Username:</label><br><input type="text" id="username" name="username"><br><input type="submit" value="Submit"></form> ')
+            res.write('</html>');
+            return res.end();
+
+    }else if(url=='/users'){
         res.write('<html>');
-        res.write('<head><title>My first page</title>></head>');
-        res.write('<body><form action="/message" method="POST"><input type ="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<head><title>Hi</title></head>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>User 1</li>');
+        res.write('<li>User 2</li>');
+        res.write('<li>User 3</li>');
+        res.write('<li>User 4</li>');
+        res.write('<li>User 5</li>');
+        res.write('</ul>');
+        res.write('</body>');
         res.write('</html>');
-       return  res.end()
-    }
-
-
-    if(url==='/message' && method ==='POST'){
-        const body= [];
+        return res.end();
+    }else if(url=='/create-user'&&method=='POST'){
+        const body=[];
         req.on('data',(chunk)=>{
-            console.log(chunk);
             body.push(chunk);
         });
         req.on('end',()=>{
-            const parsedBody= Buffer.concat(body).toString();
-            console.log(parsedBody);
+            const parsed=Buffer.concat(body).toString();
+            console.log(parsed);
         });
-        fs.writeFileSync('message.txt','DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
-}
+};
 
-module.exports=requestHandler;
-   
+module.exports=serverRequestHandler;

5fa8e8efb0ae4119163d6a19b075b76a324f120b
roshankumar <roshan.k@toppr.com>
2023-01-16 00:01:58 +0530

    learned how to pass data from js to pug

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const adminData=require('./admin');
 router.get('/',(req,res,next)=>{
    const products= adminData.products;
     //This is provided by express to render
-    res.render('shop',{prods:products,docTitle:'Shop'});
+    res.render('shop',{prods:products,docTitle:'Shop',path:'/'});
     //res.sendFile(path.join(rootDir,'views','shop.pug'));
 });
 

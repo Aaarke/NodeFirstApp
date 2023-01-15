5fcca82122f253aef02928d1a202cc88f52cd7b2
roshankumar <roshan.k@toppr.com>
2023-01-16 00:11:41 +0530

    finished learning pug templete engine

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res)=>{
-    res.status(404).render('notfound');
+    res.status(404).render('notfound',{pageTitle:'Page Not Found'});
     //res.status(404).sendFile(path.join(__dirname,'views','notfound.html'));
 
 });
